d8ffceb39ecac79bc664131b0fd99a5aa60f37b4
codetective <iaminiodu@gmail.com>
2022-09-23 13:00:15 +0100

    donw with profile page ui

---
+++ src/components/DashBoard/PhoneNumberInput.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Flex,
+  Icon,
+  Input,
+  Select,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import Flag from "react-world-flags";
+import { AsYouType } from "libphonenumber-js";
+import { getCountryTelCode } from "../../utils/countries";
+import { FaPhone, FaChevronDown } from "react-icons/fa";
+
+export default function PhoneNumberInput({
+  size,
+  value,
+  country,
+  options,
+  onChange,
+  placeholder,
+  ...rest
+}) {
+  let [number, setNumber] = useState(value || "");
+  let [selectedCountry, setSelectedCountry] = useState(country);
+  let [countryCode, setCountryCode] = useState(
+    getCountryTelCode(country) || ""
+  );
+
+  useEffect(() => {
+    setSelectedCountry(country);
+    setCountryCode(getCountryTelCode(country));
+  }, [country]);
+
+  const onCountryChange = (e) => {
+    let value = e.target.value;
+    let code = getCountryTelCode(value);
+    let parsedNumber = new AsYouType().input(`${code}${number}`);
+
+    setCountryCode(code);
+    setSelectedCountry(value);
+    onChange(parsedNumber);
+  };
+
+  const onPhoneNumberChange = (e) => {
+    let value = e.target.value;
+    let parsedNumber = new AsYouType().input(`${countryCode}${value}`);
+
+    setNumber(value);
+    onChange(parsedNumber);
+  };
+
+  return (
+    <InputGroup size={size} {...rest}>
+      <InputLeftElement width="4rem">
+        <Select
+          top="0"
+          left="0"
+          zIndex={1}
+          bottom={0}
+          opacity={0}
+          height="40px"
+          position="absolute"
+          value={selectedCountry}
+          onChange={onCountryChange}
+          icon={<></>}
+        >
+          <option value="" />
+          {options.map((option, indx) => (
+            <option key={indx} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </Select>
+        <Flex pl={2} width="100%" alignItems="center">
+          {selectedCountry ? (
+            <Box mr="4px" width="50%" flex={1}>
+              <Flag height="1rem" code={selectedCountry || ""} />
+            </Box>
+          ) : (
+            <Icon as={FaPhone} />
+          )}
+          <Icon as={FaChevronDown} />
+        </Flex>
+      </InputLeftElement>
+      <Input
+        pl="4rem"
+        type="tel"
+        value={number}
+        pattern="[0-9]"
+        placeholder={placeholder}
+        onChange={onPhoneNumberChange}
+        name="phone"
+      />
+    </InputGroup>
+  );
+}
+
+PhoneNumberInput.defaultProps = {
+  options: [],
+  size: "md",
+};
--- src/components/ProfilePage/ProfileDetailsSection.jsx
+++ src/components/ProfilePage/ProfileDetailsSection.jsx
@@ -1,15 +1,29 @@
-import { FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
-import React from "react";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Select,
+  Stack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { COUNTRIES } from "../../utils/countries";
 import CustomBox from "../DashBoard/CustomBox";
+import PhoneNumberInput from "../DashBoard/PhoneNumberInput";
 import SectionHeading from "../DashBoard/SectionHeading";
 
 function ProfileDetailsSection() {
+  const countryOptions = COUNTRIES.map(({ name, iso }) => ({
+    label: name,
+    value: iso,
+  }));
+  const [phone_number, setPhoneNumber] = useState("");
   return (
     <CustomBox flex="2" mr={[0, 0, 5]}>
       <SectionHeading fontWeight="semibold" fontSize="md">
         Profile Details
       </SectionHeading>
-      <Stack as="form" pt="10" spacing="5">
+      <Stack as="form" pt="10" spacing="6">
         <FormControl>
           <FormLabel>First name</FormLabel>
           <Input type="text" name="first_name" placeholder="first name" />
@@ -17,7 +31,67 @@ function ProfileDetailsSection() {
         {/* last */}
         <FormControl>
           <FormLabel>Last name</FormLabel>
-          <Input type="text" name="first_name" placeholder="last name" />
+          <Input type="text" name="last_name" placeholder="last name" />
+        </FormControl>
+        <FormControl required>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            type="email"
+            name="email"
+            placeholder="enter valid email address"
+          />
+        </FormControl>
+        <FormControl mb="32px">
+          <FormLabel>Phone Number</FormLabel>
+          <PhoneNumberInput
+            value={phone_number}
+            options={countryOptions}
+            placeholder="Enter phone number"
+            onChange={(value) => setPhoneNumber(value)}
+            country={"NGA"}
+          />
+          <FormHelperText>{phone_number}</FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>What's your gender?</FormLabel>
+
+          <Select name="gender">
+            <option value={null} disabled selected>
+              Select gender
+            </option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Nationality</FormLabel>
+          <Input
+            type="text"
+            name="nationality"
+            placeholder="enter nationality"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Address Line 1</FormLabel>
+          <Input type="text" name="address_1" placeholder="enter address" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Address Line 2 (Optional)</FormLabel>
+          <Input
+            type="text"
+            name="address_2"
+            placeholder="enter address 2 - optional"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>City</FormLabel>
+          <Input type="text" name="city" placeholder="enter city" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Zip/Post code</FormLabel>
+          <Input type="number" name="zip_code" placeholder="enter zip code" />
         </FormControl>
       </Stack>
     </CustomBox>
